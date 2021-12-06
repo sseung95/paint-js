53b4c871a60fa8d21fc2b66e5a6b082a3b8eedb8
sseung95 <seung3657@gmail.com>
2021-12-06 16:35:06 +0900

    Feat: 선 굵기, 색상, 모드 변경 및 이미지 저장 기능 추가

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelector('#jsColors');
+const range = document.querySelector('#jsRange');
+const mode = document.querySelector('#jsMode');
+const saveBtn = document.querySelector('#jsSave');
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 500;
 
-ctx.strokeStyle = '#2c2c2c';
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE); // 캔버스 배경색상 흰색으로 초기 설정
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startPainting() {
   painting = true;
@@ -30,9 +41,67 @@ function onMouseMove(event) {
   }
 }
 
+function changeColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function changeLineWidth(event) {
+  const lineWidth = event.target.value;
+  ctx.lineWidth = lineWidth;
+}
+
+function changeMode(event) {
+  if (filling) {
+    filling = false;
+    event.target.innerText = 'Fill';
+  } else {
+    filling = true;
+    event.target.innerText = 'Paint';
+  }
+}
+
+function canvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+/* 오른쪽 클릭 방지 */
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function saveImage() {
+  const image = canvas.toDataURL(); // 기본 png 설정
+  const link = document.createElement('a');
+  link.href = image; // href 속성은 컨버스의 데이터 URL
+  link.download = 'PaintJS'; // download 속성은 파일명
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('click', canvasClick);
+  canvas.addEventListener('contextmenu', handleCM);
+}
+
+if (colors) {
+  colors.addEventListener('click', changeColor);
+}
+
+if (range) {
+  range.addEventListener('change', changeLineWidth);
+}
+
+if (mode) {
+  mode.addEventListener('click', changeMode);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', saveImage);
 }
